2639b780c433e0d0cb4efd5df6a9c01ce6ce4c69
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-10-27 18:49:47 +0100

    Add default values for config file, change expected toml file name

--- src/config.rs
+++ src/config.rs
@@ -3,35 +3,57 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct CaSettings {
-    pub common_name: String,
-    pub expiry_in_days: u32
+  pub common_name: String,
+  pub expiry_in_days: u32
 }
 
 #[derive(Debug, Deserialize)]
 pub struct PkiSettings {
-    // Shared x509 Attributes
-    pub country_name: String,
-    pub locality: String,
-    pub organization: String,
-    pub organizational_unit: String,
-    pub state: String,
-    pub email_address: String, 
+  // Shared x509 Attributes
+  pub country_name: String,
+  pub locality: String,
+  pub organization: String,
+  pub organizational_unit: String,
+  pub state: String,
+  pub email_address: String, 
 
-    pub rsa_size: u32,
-    pub ca: CaSettings
+  pub rsa_size: u32,
+  pub ca: CaSettings
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(default)]
 pub struct Settings {
-    pub debug: bool,
-    pub pki: PkiSettings
+  pub debug: bool,
+  pub pki: PkiSettings
 }
 
-
 impl Settings {
-    pub fn new() -> Result<Self, ConfigError> {
-        let mut a = Config::new();
-        a.merge(File::with_name("default.toml"))?;
-        a.try_into()
+  pub fn new() -> Result<Self, ConfigError> {
+    let mut cfg = Config::new();
+    cfg.merge(File::with_name("rusty-sailor.toml"))?;
+    cfg.try_into()
+  }
+}
+
+impl Default for Settings {
+  fn default() -> Self {
+    Settings {
+      debug: true,
+      pki: PkiSettings {
+        country_name: "PL".to_string(),
+        locality: "Gdansk".to_string(),
+        organization: "Rusty sailors ltd.".to_string(),
+        organizational_unit: "R&D".to_string(),
+        state: "Pomorskie".to_string(),
+        email_address: "rust-sailor@k8s.eu".to_string(),
+    
+        rsa_size: 4096,
+        ca: CaSettings {
+          common_name: "rusty-sailor-ca".to_string(),
+          expiry_in_days: 3650
+        }
+      }
     }
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,5 @@ fn main() {
     println!("Pki.state: {}", x.pki.state);
     println!("Pki.ca.common_name: {}", x.pki.ca.common_name);
     println!("Pki.ca.expiry_in_days: {}", x.pki.ca.expiry_in_days);
-
-    let result = rusty_sailor::pki::create_ca_certificate(&x.pki);
-    if let Ok((private_key, ca_cert)) = result {
-      rusty_sailor::pki::save_as_pem_private_key(&private_key, &"ca.key.pem".to_string());
-      rusty_sailor::pki::save_as_pem_certificate(&ca_cert, &"ca.pem".to_string());
-
-      let second_result = rusty_sailor::pki::create_ca_signed_certificate(
-        &x.pki,
-        &private_key,
-        &ca_cert,
-        &"blackwood".to_string(),
-        &13,
-        &Some(vec!["blackwood.local".to_string()]),
-        &Some(vec!["127.0.0.1".to_string()])
-      );
-
-      if let Ok((pkey, cert)) = second_result {
-        rusty_sailor::pki::save_as_pem_private_key(&pkey, &"blackwood.key.pem".to_string());
-        rusty_sailor::pki::save_as_pem_certificate(&cert, &"blackwood.pem".to_string());
-      }
-    }
   }
 }
