176313e4f304d9418beacc53fc692c14ffb0d8be
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-11-06 16:10:59 +0100

    Ensure etcd creates dedicated dir for itself

--- src/components/etcd.rs
+++ src/components/etcd.rs
@@ -1,8 +1,19 @@
+use std::fs::create_dir_all;
+use std::path::Path;
+
 use crate::components::InstallStepResult;
 use crate::install_ctx::InstallCtx;
+
+const ETCD_DIRNAME: &'static str = "etcd";
 
 pub fn etcd_component(
   mut install_ctx: InstallCtx
 ) -> InstallStepResult {
+  let target_dir = Path::new(
+    &install_ctx.config.installation_dir
+  ).join(ETCD_DIRNAME);
+
+  create_dir_all(&target_dir)?;
+
   Ok(install_ctx)
 }
