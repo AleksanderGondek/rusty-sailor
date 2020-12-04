ddaa7b222f25c30537a47982e012cd6aaa6851a7
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-12-04 19:09:18 +0100

    Etcd component - update etcd to version 3.4.14

--- vendored.nix
+++ vendored.nix
@@ -6,8 +6,8 @@ let
   ) {};
   etcd_pkg = pkgs.fetchurl {
     name = "etcd";
-    url = "https://github.com/etcd-io/etcd/releases/download/v3.4.13/etcd-v3.4.13-linux-amd64.tar.gz";
-    sha256 = "2ac029e47bab752dacdb7b30032f230f49e2f457cbc32e8f555c2210bb5ff107";
+    url = "https://github.com/etcd-io/etcd/releases/download/v3.4.14/etcd-v3.4.14-linux-amd64.tar.gz";
+    sha256 = "0l8nc1hhhw81v68bw2chnp92msili6wx1g7hwa27lwpbbka42wgm";
   };
 in
 pkgs.stdenv.mkDerivation({
