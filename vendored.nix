6d7b8b42097e25e07678ef75783815cb32ffc025
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-12-07 18:12:14 +0100

    Containerd - add to project assets

--- vendored.nix
+++ vendored.nix
@@ -4,6 +4,11 @@ let
   pkgs = import (
     fetchTarball { url = https://github.com/NixOS/nixpkgs/archive/nixos-20.09.tar.gz;}
   ) {};
+  containerd_pkg = pkgs.fetchurl {
+    name = "containerd";
+    url = "https://github.com/containerd/containerd/releases/download/v1.4.3/containerd-1.4.3-linux-amd64.tar.gz";
+    sha256 = "1wa2apxcknnj28hffbwj274camhsxfg366hl0dxk7zarykin389l";
+  };
   etcd_pkg = pkgs.fetchurl {
     name = "etcd";
     url = "https://github.com/etcd-io/etcd/releases/download/v3.4.14/etcd-v3.4.14-linux-amd64.tar.gz";
@@ -17,5 +22,6 @@ pkgs.stdenv.mkDerivation({
   installPhase = ''
     mkdir -p $out
     cp ${etcd_pkg} "$out/${etcd_pkg.name}.tar.gz"
+    cp ${containerd_pkg} "$out/${containerd_pkg.name}.tar.gz"
   '';
 })
