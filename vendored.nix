ad0411729dc58aec43ba0deb84f9ddcaaac95959
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-12-29 18:29:37 +0100

    Containerd - pack only binaries entourage

--- vendored.nix
+++ vendored.nix
@@ -6,8 +6,8 @@ let
   ) {};
   containerd_pkg = pkgs.fetchurl {
     name = "containerd";
-    url = "https://github.com/containerd/containerd/releases/download/v1.4.3/containerd-1.4.3-linux-amd64.tar.gz";
-    sha256 = "1wa2apxcknnj28hffbwj274camhsxfg366hl0dxk7zarykin389l";
+    url = "https://github.com/containerd/containerd/releases/download/v1.4.3/cri-containerd-cni-1.4.3-linux-amd64.tar.gz";
+    sha256 = "0ggz5fl517smd87346gdblgssckyzmcy44w3nhd22z27wd1a75r6";
   };
   etcd_pkg = pkgs.fetchurl {
     name = "etcd";
@@ -21,7 +21,22 @@ pkgs.stdenv.mkDerivation({
   phases = [ "installPhase" ];
   installPhase = ''
     mkdir -p $out
+    # Etcd binaries
     cp ${etcd_pkg} "$out/${etcd_pkg.name}.tar.gz"
-    cp ${containerd_pkg} "$out/${containerd_pkg.name}.tar.gz"
+
+    # Containerd binaries with
+    # unfortunate repacking
+    cp ${containerd_pkg} ./cni-containerd.tar.gz
+    mkdir -p cni-containerd-unpacked
+    mkdir -p containerd
+
+    tar -xvf ./cni-containerd.tar.gz -C ./cni-containerd-unpacked
+    cp ./cni-containerd-unpacked/usr/local/bin/* ./containerd
+    cp ./cni-containerd-unpacked/usr/local/sbin/* ./containerd
+    cp ./cni-containerd-unpacked/opt/cni/bin/* ./containerd
+
+    cd ./containerd
+    tar -czvf $out/${containerd_pkg.name}.tar.gz .
+    cd ..
   '';
 })
